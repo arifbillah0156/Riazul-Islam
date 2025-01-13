0de4deb2b42cedf6d34f9e2552c24e34a99dbed2
arifbillah0156 <arifbillah0156@gmail.com>
2025-01-13 17:26:24 +0600

    second

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white">{children}</body>
+      <body className="bg-white merriweather-regular">{children}</body>
     </html>
   );
 }
